42d1abb84aefce24ab27f9cfe4b6f9fae5adc663
kinemakki <32955750+fast-slow@users.noreply.github.com>
2021-05-19 13:34:25 +0900

    Add files via upload

---
+++ server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+print("serving at port", PORT)
+httpd.serve_forever()
